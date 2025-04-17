f5f1718cdfa2e53ddea54696b16f2459f9d84087
ghanigh <rhani.94@proton.me>
2025-04-17 21:01:28 +0200

    exo

---
+++ helloworld.cob
@@ -0,0 +1,8 @@
+           IDENTIFICATION DIVISION.
+           PROGRAM-ID. SALUTLEPEUPLE.
+           AUTHOR. GHANI.
+           DISPLAY. "Salut tout le monde cest LaSalle".
+           STOP RUN.
+
+
+           
